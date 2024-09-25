e099f9436ddb90e6e9655a46dd91bf97319d8698
Alexander Bays <bays@956mb.com>
2024-09-25 18:10:45 -0500

    use telescope by default
    - added: `NcksSearch` command for searching list
    - added: checks if ncks file (default: ~/.ncks) exists, asks to create
    it

--- README.md
+++ README.md
@@ -18,11 +18,15 @@ This is my first [Neovim](https://github.com/neovim/neovim) plugin, and it's jus
 ```lua
 {
     "956MB/ncks.nvim",
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+    },
     config = function()
         require('ncks').setup {} -- required
 
         vim.keymap.set('n', '<leader>nn', '<cmd>NcksNew<cr>', { desc = 'Add [N]ew [N]ickname' })
-        vim.keymap.set('n', '<leader>ni', '<cmd>NcksInfo<cr>', { desc = 'Show [N]icks file [I]nfo' })
+        vim.keymap.set('n', '<leader>ni', '<cmd>NcksInfo<cr>', { desc = 'Show [N]cks file [I]nfo' })
+        vim.keymap.set('n', '<leader>ns', '<cmd>NcksSearch<cr>', { desc = 'Search [N]cks file' })
         vim.keymap.set('n', '<leader>no', '<cmd>NcksOpen<cr>', { desc = '[O]pen [N]cks file' })
         vim.keymap.set('n', '<leader>nl', '<cmd>NcksList<cr>', { desc = '[L]ist [N]cks files' })
         vim.keymap.set('n', '<leader>nc', '<cmd>NcksCopyAll<cr>', { desc = '[C]opy all [N]icknames from file to clipboard' })
@@ -35,6 +39,8 @@ This is my first [Neovim](https://github.com/neovim/neovim) plugin, and it's jus
 
 - `:NcksInfo`
     - Displays various information on ncks file, like ***file location*** and ***entry count***.
+- `:NcksSearch`
+    - Search nicknames and copy selection to clipboard
 - `:NcksOpen`
     - Opens ncks file in new buffer.
 - `:NcksCopyAll`
@@ -49,102 +55,40 @@ This is my first [Neovim](https://github.com/neovim/neovim) plugin, and it's jus
 
 ## Configuration
 
-### Defaults
-
 ```lua
-┌─ New (~/.ncks) ───────────────────────┐
-│ ...                                 * │ -- Without preview (Default)
-└───────────────────────────────────────┘
-
--- ...
-
 {
     -- Default file ~/.ncks, but can use literally anything
     location = '~/.ncks',
-    -- Layout config stuff if you want Telescope
+    -- Telescope layout configuration
     layout_config = {
         prompt_position = 'top',
         width = 0.50,
         height = 0.20,
     },
-    prompt_title = 'New',
-}
-```
-
-### Telescope
-
-```lua
-┌───────────────── New ─────────────────┐
-│ ...                           12 / 12 │
-├─────────────── ~/.ncks ───────────────┤
-│ BATT_MODEL_RAW                        │
-│ conductor galloping spans             │
-│ L812 Electric Two-Sided Grill         │ -- With preview (Telescope)
-│ lackof(grid)                          │
-│ ...                                   │
-└───────────────────────────────────────┘
-
--- ...
-
-{   -- Using telescope.nvim
-
-    local ncks = require 'ncks'
-    ncks.setup {} -- required
-
-    local function toggle_telescope(contents)
-        local function handle_input(prompt_bufnr)
-            local entry = require('telescope.actions.state').get_current_line()
-            if entry and entry ~= '' then
-                require('telescope.actions').close(prompt_bufnr)
-                ncks.write(entry)
-            end
-        end
-
-        require('telescope.pickers')
-            .new({
-                prompt_title = ncks.config.prompt_title,
-                results_title = ncks.config.location,
-                finder = require('telescope.finders').new_table {
-                    results = contents,
-                    entry_maker = function(entry)
-                        return { value = entry, display = entry, ordinal = entry, }
-                    end,
-                },
-                layout_config = ncks.config.layout_config,
-                sorting_strategy = 'ascending',
-                attach_mappings = function(prompt_bufnr, map)
-                    map('i', '<CR>', function() handle_input(prompt_bufnr) end)
-                    map('n', '<CR>', function() end)
-                    return true
-                end,
-            }, {})
-            :find()
-    end
-
-    -- ... keymaps
-
-    vim.keymap.set('n', '<leader>nn', function()
-        ncks.exists(function(_)
-            toggle_telescope(ncks.list())
-        end)
-    end, { desc = 'Open ncks window' })
+    -- Default settings
+    telescope_defaults = {
+        include_location = true, -- Include file location in prompt title
+        selection_caret = '┃ ',
+    },
+    new_nickname = {
+        prompt_title = 'New Nickname',
+        prompt_prefix = '   ', -- Plus icon for adding new nickname
+    },
+    search = {
+        prompt_title = 'Search Nicknames',
+        prompt_prefix = '   ', -- Search icon for searching nicknames
+    },
 }
 ```
 
 ## Aknowledgements
 
-Both [`ThePrimeagen/harpoon`](https://github.com/ThePrimeagen/harpoon/tree/harpoon2) and [`tamton-aquib/stuff.nvim`](https://github.com/tamton-aquib/stuff.nvim) served as inspiration and helped me build up the structure of this tiny plugin. (The Telescope thing is straight out of harpoon TBH)
+Both [`ThePrimeagen/harpoon`](https://github.com/ThePrimeagen/harpoon/tree/harpoon2) and [`tamton-aquib/stuff.nvim`](https://github.com/tamton-aquib/stuff.nvim) served as inspiration and helped me build up the structure of this tiny plugin.
 
 ## TODO
 
-- [ ] If file doesn't exist when trying to open or add, ask to create it
-- [ ] Still confused on how to fine tune all of Telescope settings
-    - [ ] Line count in the prompt shouldn't be "9 / 9"
-    - [ ] Figure out how to use Telescope ui without the results window, just the prompt
 - [ ] Multiple lists?
-- [ ] More commands:
-    - [ ] Create, Delete, Move/Rename, etc.
-- [ ] Tests?
+- [ ] More commands (delete, edit, etc):
 
 ## License
 
--- lua/ncks/init.lua
+++ lua/ncks/init.lua
@@ -2,13 +2,38 @@ local M = {}
 
 local config = {
     location = '~/.ncks',
+    use_telescope = true,
     layout_config = {
         prompt_position = 'top',
         width = 0.50,
         height = 0.20,
     },
-    prompt_title = 'New Nickname',
+    telescope_defaults = {
+        include_location = true,
+        selection_caret = '┃ ',
+    },
+    new_nickname = {
+        prompt_title = 'New Nickname',
+        prompt_prefix = '   ',
+    },
+    search = {
+        prompt_title = 'Search Nicknames',
+        prompt_prefix = '   ',
+    },
 }
+
+local function ensure_telescope()
+    local ok, _ = pcall(require, 'telescope')
+    return ok
+end
+
+local function get_prompt_title(base_title)
+    if M.config.telescope_defaults.include_location then
+        return string.format('%s (%s)', base_title, M.config.location)
+    else
+        return base_title
+    end
+end
 
 function M.setup(opts)
     M.config = vim.tbl_deep_extend('force', config, opts or {})
@@ -33,24 +58,83 @@ function M.write(nck)
 end
 
 function M.exists(callback)
-    local path = M.config.location
-    if vim.loop.fs_stat(vim.fn.expand(path)) then
+    local path = vim.fn.expand(M.config.location)
+
+    if vim.fn.filereadable(path) == 1 then
         callback(path)
+        return true
     else
-        vim.api.nvim_err_writeln('err: Ncks file (' .. path .. ") doesn't exist.")
+        local create = vim.fn.confirm("Nicknames file doesn't exist. (location: " .. M.config.location .. ') Create it?', '&Yes\n&No', 1)
+        if create == 1 then
+            local file = io.open(path, 'w')
+            if file then
+                file:close()
+                print('Created nicknames file: ' .. path)
+                callback(path)
+                return true
+            else
+                vim.api.nvim_err_writeln('Failed to create nicknames file: ' .. path)
+            end
+        else
+            print 'Nicknames file not created. Command aborted.'
+        end
     end
+
+    return false
 end
 
 function M.new(nck)
-    M.exists(function(path)
+    local function toggle_telescope(contents)
+        local function handle_input(prompt_bufnr)
+            local entry = require('telescope.actions.state').get_current_line()
+            if entry and entry ~= '' then
+                require('telescope.actions').close(prompt_bufnr)
+                M.write(entry)
+            end
+        end
+
+        require('telescope.pickers')
+            .new({
+                prompt_prefix = M.config.new_nickname.prompt_prefix,
+                selection_caret = M.config.telescope_defaults.selection_caret,
+                prompt_title = get_prompt_title(M.config.new_nickname.prompt_title),
+                results_title = M.config.location,
+                finder = require('telescope.finders').new_table {
+                    results = contents,
+                    entry_maker = function(entry)
+                        return {
+                            value = entry,
+                            display = entry,
+                            ordinal = entry,
+                        }
+                    end,
+                },
+                layout_config = M.config.layout_config,
+                sorting_strategy = 'ascending',
+                attach_mappings = function(prompt_bufnr, map)
+                    map('i', '<CR>', function()
+                        handle_input(prompt_bufnr)
+                    end)
+                    map('n', '<CR>', function() end)
+                    return true
+                end,
+            }, {})
+            :find()
+    end
+
+    M.exists(function(_)
         if not nck or #nck == 0 then
-            vim.ui.input({
-                prompt = M.config.prompt_title .. ' (' .. path .. ')',
-            }, function(input)
-                if input then
-                    M.write(input)
-                end
-            end)
+            if ensure_telescope() then
+                toggle_telescope(M.list())
+            else
+                vim.ui.input({
+                    prompt = get_prompt_title(M.config.new_nickname.prompt_title),
+                }, function(input)
+                    if input then
+                        M.write(input)
+                    end
+                end)
+            end
         else
             M.write(nck)
         end
@@ -74,6 +158,72 @@ function M.list()
     end)
 
     return lines
+end
+
+function M.search()
+    if ensure_telescope() then
+        local function copy_to_clipboard(prompt_bufnr)
+            local selection = require('telescope.actions.state').get_selected_entry()
+            if selection then
+                vim.fn.setreg('+', selection.value)
+                require('telescope.actions').close(prompt_bufnr)
+                print('Copied to clipboard: ' .. selection.value)
+            end
+        end
+
+        M.exists(function()
+            require('telescope.pickers')
+                .new({
+                    prompt_prefix = M.config.search.prompt_prefix,
+                    selection_caret = M.config.telescope_defaults.selection_caret,
+                    prompt_title = get_prompt_title(M.config.search.prompt_title),
+                    finder = require('telescope.finders').new_table {
+                        results = M.list(),
+                        entry_maker = function(entry)
+                            return {
+                                value = entry,
+                                display = entry,
+                                ordinal = entry,
+                            }
+                        end,
+                    },
+                    sorter = require('telescope.sorters').get_generic_fuzzy_sorter(),
+                    attach_mappings = function(_, map)
+                        map('i', '<CR>', function(pb)
+                            copy_to_clipboard(pb)
+                        end)
+                        map('n', '<CR>', function(pb)
+                            copy_to_clipboard(pb)
+                        end)
+                        return true
+                    end,
+                    layout_config = M.config.layout_config,
+                }, {})
+                :find()
+        end)
+    else
+        M.exists(function()
+            local nicknames = M.list()
+            if #nicknames == 0 then
+                print 'No nicknames found.'
+                return
+            end
+            for i, nick in ipairs(nicknames) do
+                print(string.format('%d. %s', i, nick))
+            end
+            vim.ui.input({ prompt = 'Enter number to copy (or q to quit): ' }, function(input)
+                if input and input ~= 'q' then
+                    local num = tonumber(input)
+                    if num and nicknames[num] then
+                        vim.fn.setreg('+', nicknames[num])
+                        print('Copied to clipboard: ' .. nicknames[num])
+                    else
+                        print 'Invalid selection.'
+                    end
+                end
+            end)
+        end)
+    end
 end
 
 function M.random()
@@ -114,11 +264,20 @@ function M.info()
 end
 
 local function command(name, func, opts)
-    return { name = name, func = func, opts = opts }
+    return {
+        name = name,
+        func = function(cmd_opts)
+            M.exists(function(_)
+                func(cmd_opts)
+            end)
+        end,
+        opts = opts,
+    }
 end
 
 local commands = {
     command('NcksInfo', M.info, { desc = 'Display information about the ncks file' }),
+    command('NcksSearch', M.search, { desc = 'Search nicknames and copy selection to clipboard' }),
     command('NcksOpen', M.open, { desc = 'Open the ncks file in a new buffer' }),
     command('NcksCopyAll', M.copy_all, { desc = 'Copy all entries in the ncks file to clipboard' }),
     command('NcksRandom', M.random, { desc = 'Pick a random entry from the ncks file and copy it to clipboard' }),
